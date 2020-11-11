ff6f616d28de2bdff5f89a06edaf0cc4c7b858c3
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-11-11 22:11:18 +0530

    added dish category list

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   static const String id = 'home_page';
   @override
   _HomePageState createState() => _HomePageState();
+}
+
+/* This method is to setup each dishCategory */
+Container _dishCategory(String text) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 10.5),
+    decoration: BoxDecoration(
+      border: Border.all(color: Colors.grey[400]),
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      color: Colors.white,
+    ),
+    child: Text(text),
+  );
 }
 
 class _HomePageState extends State<HomePage> {
@@ -18,8 +32,23 @@ class _HomePageState extends State<HomePage> {
 
       /* Creating Safe Area so that the content stays inside the screen */
       body: SafeArea(
-        child: Container(
-          color: Colors.teal,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8.0),
+              color: Color(0xffEBEDEC),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  /* Calling the _dishCategory method at line 10 */
+                  _dishCategory('Indian'),
+                  _dishCategory('Italian'),
+                  _dishCategory('Shake'),
+                  _dishCategory('South Indian'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
