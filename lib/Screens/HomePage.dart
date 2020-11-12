38cde2c03867e3226f62b9cdf07c0c54454626d1
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-11-12 05:40:04 +0530

    finished recipe feed

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -36,6 +36,7 @@ Container _post(
           children: <Widget>[
             Icon(Icons.account_circle),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(name),
                 Text(time),
@@ -105,22 +106,40 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Container(
-              child: Column(
-                children: <Widget>[
-                  /* Calling the _post method at line 23 */
-                  _post(
-                      name: 'FoodFood',
-                      time: '28 mins',
-                      description:
-                          'A bite of Bhatura, just the size, oozing with the '
-                          'chole is pure decadence The flavour burst in '
-                          'your mouth, hot at first, tangy after. \n\n'
-                          'See full recipe...See more',
-                      image: Image.asset('assets/images/chole_bhature.jpg'),
-                      likes: '18,550',
-                      comments: 113,
-                      width: _width),
-                ],
+              child: Expanded(
+                child: ListView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    /* Calling the _post method at line 23 */
+                    _post(
+                        name: 'FoodFood',
+                        time: '28 mins',
+                        description:
+                            'A bite of Bhatura, just the size, oozing with the '
+                            'chole is pure decadence The flavour burst in '
+                            'your mouth, hot at first, tangy after. \n\n'
+                            'See full recipe...See more',
+                        image: Image.asset('assets/images/chole_bhature.jpg'),
+                        likes: '18,550',
+                        comments: 113,
+                        width: _width),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    _post(
+                        name: 'FoodFood',
+                        time: '1 Nov at 12:44 pm',
+                        description: '#SizzlingSunday Ab luft uthaiye dal ke'
+                            'saath gosht ka! Try this robust & rich Dal Gosht'
+                            'recipe, It\'ll surely you\'re your heart! \n\n'
+                            'Watch full recipe...See more',
+                        image: Image.asset('assets/images/dal_gosht.jpg'),
+                        likes: '317',
+                        comments: 21,
+                        width: _width),
+                  ],
+                ),
               ),
             ),
           ],
