88680c9da0301b509341007b4aa242fecde88220
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-11-20 10:31:47 +0530

    added dummy way to show ingredients and steps

--- lib/Screens/MakeRecipePage.dart
+++ lib/Screens/MakeRecipePage.dart
@@ -12,6 +12,28 @@ class _MakeRecipesPageState extends State<MakeRecipesPage> {
   Widget build(BuildContext context) {
     final RecipeArgument = ModalRoute.of(context).settings.arguments;
     //TODO: here is one more arg of list in a list 0 wala ingredients aur 1 wala steps
+
+    /* This is how I was thinking of adding steps and ingredients */
+    String ingredients = '2 cups white sugar\n1 1⁄4 cups vegetable oil\n'
+        '1 teaspoon vanilla extract\n2 cups canned pumpkin'
+        '\n4 eggs\n2 cups all-purpose flour\n'
+        '3 teaspoons baking powder\n'
+        '2 teaspoons baking soda\n'
+        '1⁄4 teaspoon salt\n'
+        '2 teaspoons ground cinnamon\n'
+        '1 cup chopped walnuts (Optional)';
+    String steps = 'Step 1\nPreheat oven to 350 degrees F (175 degrees C).'
+        ' Grease and flour a 12x18 inch pan. Sift together'
+        ' the flour, baking powder, baking soda, salt and'
+        ' cinnamon. Set aside.\n\nStep 2\nIn a large bowl'
+        ' combine sugar and oil. Blend in vanilla and '
+        'pumpkin, then beat in eggs one at a time. '
+        'Gradually beat in flour mixture. Stir in nuts. '
+        'Spread batter into prepared 12x18 inch pan.\n\n'
+        'Step 3\nBake in the preheated oven for 30 minutes,'
+        ' or until a toothpick inserted into the centre of '
+        'the cake comes out clean. Allow to cool.';
+
     double _width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
@@ -31,14 +53,7 @@ class _MakeRecipesPageState extends State<MakeRecipesPage> {
             color: Colors.grey,
           ),
           Container(
-            child: Text('2 cups white sugar\n1 1⁄4 cups vegetable oil\n'
-                '1 teaspoon vanilla extract\n2 cups canned pumpkin'
-                '\n4 eggs\n2 cups all-purpose flour\n'
-                '3 teaspoons baking powder\n'
-                '2 teaspoons baking soda\n'
-                '1⁄4 teaspoon salt\n'
-                '2 teaspoons ground cinnamon\n'
-                '1 cup chopped walnuts (Optional)'),
+            child: Text(ingredients),
           ),
           Text('Direction'),
           Container(
@@ -49,17 +64,7 @@ class _MakeRecipesPageState extends State<MakeRecipesPage> {
             color: Colors.grey,
           ),
           Container(
-            child: Text('Step 1\nPreheat oven to 350 degrees F (175 degrees C).'
-                ' Grease and flour a 12x18 inch pan. Sift together'
-                ' the flour, baking powder, baking soda, salt and'
-                ' cinnamon. Set aside.\n\nStep 2\nIn a large bowl'
-                ' combine sugar and oil. Blend in vanilla and '
-                'pumpkin, then beat in eggs one at a time. '
-                'Gradually beat in flour mixture. Stir in nuts. '
-                'Spread batter into prepared 12x18 inch pan.\n\n'
-                'Step 3\nBake in the preheated oven for 30 minutes,'
-                ' or until a toothpick inserted into the centre of '
-                'the cake comes out clean. Allow to cool.'),
+            child: Text(steps),
           ),
         ],
       ),
