c3ad9ba267b934b4232472ec9de8629b849d9d7a
ayushi-sharma-123 <sharmaa14001@gmail.com>
2020-11-26 23:28:36 +0530

    added cloud services

---
+++ lib/Firestore/CloudFirestore.dart
@@ -0,0 +1,97 @@
+import 'package:cook_chef/Firestore/CloudStorage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
+CollectionReference user;
+CollectionReference feeds;
+
+class CloudFirestore {
+  String uid = auth.currentUser.uid.toString();
+  CloudStorage _cloudStorage = CloudStorage();
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  Future<void> userSetUp(String userName) async {
+    user = FirebaseFirestore.instance.collection('Users');
+    //user.doc(uid).set({'username': userName, 'uid': uid});
+    String fcmToken = await _firebaseMessaging.getToken();
+    await user.doc(uid).set({
+      'username': userName,
+      'uid': uid,
+      'imageLink':
+          'https://firebasestorage.googleapis.com/v0/b/cook-chef.appspot.com/o/Users%2Fprofile-user.png?alt=media&token=f156591e-9aa4-4c42-8b28-0ce36eef7d5c',
+      'bio': 'Here you can add his/her bio',
+      'fcmToken': fcmToken
+    });
+    return null;
+  }
+
+  Future<void> addingPost(String recipe, File _image) async {
+    feeds = FirebaseFirestore.instance.collection('feeds');
+    Timestamp time = Timestamp.now();
+    String username = await userName();
+    DocumentReference post = await feeds.add({
+      'username': username,
+      'recipe': recipe,
+      'timestamp': time,
+      'likes': 0
+    });
+    String imageUrl = await _cloudStorage.uploadFile(_image, 'posts', post.id);
+    await feeds.doc(post.id).update({'imageUrl': imageUrl, 'postId': post.id});
+    return null;
+  }
+
+  Future<String> addingComments(String comment, String id) async {
+    String username = await userName();
+
+    DocumentReference commented = await FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(id)
+        .collection('comments')
+        .add({
+      'comment': comment,
+      'username': username,
+      'likes': 0,
+      'timestamp': Timestamp.now()
+    });
+    await FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(id)
+        .collection('comments')
+        .doc(commented.id)
+        .update({'commentId': commented.id});
+    return commented.id;
+  }
+
+  Future<void> incrementingPostLikes(String id, int like) async {
+    await FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(id)
+        .update({'likes': like + 1});
+  }
+
+  Future<void> incrementingCommentLikes(
+      String postId, int like, String commentId) async {
+    await FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(postId)
+        .collection('comments')
+        .doc(commentId)
+        .update({'likes': like + 1});
+  }
+
+  Future<void> updateUser(String username, String bio, String imageLink) async {
+    // print(await user.get());
+    //print(uid);
+    await FirebaseFirestore.instance.collection('Users').doc(uid).update(
+        {'username': username, 'bio': bio, 'uid': uid, 'imageLink': imageLink});
+  }
+
+  Future<String> userName() async {
+    DocumentSnapshot documentSnapshot =
+        await FirebaseFirestore.instance.collection('Users').doc(uid).get();
+    return documentSnapshot.data()['username'];
+  }
+}
---
+++ lib/Firestore/CloudStorage.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:cook_chef/Auth/AuthenticationService.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class CloudStorage {
+  Future<String> uploadFile(File file, String path, String name) async {
+    //File file = File(filePath);
+    //TODO: There is a need of path
+
+    try {
+      firebase_storage.TaskSnapshot uploadTask = await firebase_storage
+          .FirebaseStorage.instance
+          .ref('$path/$name')
+          .putFile(file);
+      uploadTask.ref;
+      String downloadURL = await firebase_storage.FirebaseStorage.instance
+          .ref(uploadTask.ref.fullPath)
+          .getDownloadURL();
+      return downloadURL;
+    } on firebase_core.FirebaseException catch (e) {
+      // e.g, e.code == 'canceled'
+      if (e.code == 'canceled') {
+        print('e.code');
+      }
+    }
+    return 'https://firebasestorage.googleapis.com/v0/b/cook-chef.appspot.com/o/Users%2Fprofile-user.png?alt=media&token=f156591e-9aa4-4c42-8b28-0ce36eef7d5c';
+  }
+}
